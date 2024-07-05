72d4931a1e28cb10b8207f8af31334af1be42806
Madhavaraj R <raj266864@gmail.com>
2024-07-05 12:13:02 +0530

    condition

---
+++ Javascript/Basics/Conditional.js
@@ -0,0 +1,28 @@
+// // var temperature;
+
+// // temperature = 20;
+
+// // if (temperature < 20) {
+// //   console.log("its's very cold outSide")
+// // }else{
+// //     console.log("okay");
+// // }
+
+
+// // ternary Operator
+
+// var authenticated = true;
+
+// // if (authenticated) {
+// //   console.log("show signout button");
+// // } else {
+// //   console.log("show login button");
+// // }
+
+// authenticated ? console.log("signout Button"):console.log("Login");
+
+
+// fallthrou without using break in switch
+
+
+
