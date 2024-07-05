51fb36d45e26277a2de08d2108a399200b5cf3b3
Madhavaraj R <raj266864@gmail.com>
2024-07-05 11:25:24 +0530

    varibles

---
+++ Javascript/Basics/Variables.js
@@ -0,0 +1,24 @@
+// var fullName = "Madhav";
+
+// var paymentMode;
+// console.log(paymentMode);
+// paymentMode = "UPI"
+// console.log(paymentMode);
+
+const uid = "abc123";
+
+var fullName = "Madhav";
+var email = "Madhav@gmail.com";
+var password = "123456";
+var confirmPassword = "123456";
+var count = 0;
+var isLoggedIn = false;
+
+
+console.log(uid);
+console.log("Full name is: " + fullName);
+console.log(email);
+
+
+
+
---
+++ Javascript/MapReduceFilter/index.js
@@ -0,0 +1,69 @@
+// // Map
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+
+// arr.map((item) => {
+//   console.log(item);
+// });
+
+// const users = [
+//   {
+//     name: "Madhav",
+//     age: 22,
+//   },
+//   {
+//     name: "Hari",
+//     age: 24,
+//   },{
+//     name: "Hunasi",
+//     age: 23,
+//   },{
+//     name: "Liston",
+//     age: 26,
+//   },
+// ];
+// users.map((item,key)=>{
+//     console.log(item.name,key);
+// })
+
+// Filter
+
+// const users = [
+//   {
+//     name: "Madhav",
+//     age: 22,
+//   },
+//   {
+//     name: "Hari",
+//     age: 24,
+//   },{
+//     name: "Hunasi",
+//     age: 23,
+//   },{
+//     name: "Liston",
+//     age: 26,
+//   },
+// ];
+
+// const FilteredUser = users.filter((user,index)=>{
+//     return user.name !=="Hari";
+// });
+
+// console.log(FilteredUser);
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+
+// const sum = arr.reduce((previous, current) => {
+//   return previous + current;
+// });
+// console.log(sum);
+
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     sum += arr[i];
+//   }
+// }
+// console.log(sum);
+
+
