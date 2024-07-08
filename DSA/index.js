5ccd60dcc3d77d0a221093b436b9330cf8ba8092
Madhavaraj R <raj266864@gmail.com>
2024-07-08 17:59:42 +0530

    reverse in linkedlist

--- DSA/index.js
+++ DSA/index.js
@@ -35,6 +35,86 @@ class LinkedList {
     }
     this.size++;
   }
+  removeLast() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    if (this.size === 1) {
+      const removedNode = this.head;
+      this.head = null;
+      this.tail = null;
+      this.size--;
+      return removedNode;
+    } else {
+      let curr = this.head;
+      while (curr.next.next !== null) {
+        curr = curr.next;
+      }
+      const removedNode = this.tail;
+      curr.next = null;
+      this.tail = curr;
+      this.size--;
+      return removedNode.val;
+    }
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return undefined;
+    }
+    let removed;
+    if (this.size === 1) {
+      removed = this.head;
+      this.head = null;
+      this.tail = null;
+    } else {
+      removed = this.head;
+      this.head = this.head.next;
+      this.size--;
+    }
+    return removed.val;
+  }
+
+  insert(val, index) {
+    const node = new Node(val);
+    if (index < 0 && index > this.size) {
+      return false;
+    }
+    if (index === 0) {
+      node.next = this.head;
+      this.head = node;
+      this.size++;
+      return;
+    }
+    let curr = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      curr = curr.next;
+    }
+    node.next = curr.next;
+    curr.next = node;
+    this.size++;
+    return;
+  }
+
+  reverse() {
+    if (!this.head) {
+      return undefined;
+    }
+    if (this.size === 1) {
+      return this.head;
+    }
+
+    let curr = this.head;
+    let prev = null;
+    while (curr) {
+      let temp = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = temp;
+    }
+    this.head = prev;
+  }
 
   print() {
     if (this.size > 0) {
@@ -57,6 +137,14 @@ list.AddToLast(20);
 list.AddToLast(30);
 list.AddToLast(40);
 list.AddToLast(50);
-list.AddToFirst(5)
+list.AddToFirst(5);
+console.log("removed value", list.removeLast());
+console.log("removed First", list.removeFirst());
 list.print();
+list.insert(10, 0);
+list.print();
+list.reverse()
+list.print();
+
+
 // console.log(list);
