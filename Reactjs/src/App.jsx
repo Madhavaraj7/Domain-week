161a96b338a8cdee174262bbdfac34f00c09b0b6
Madhavaraj R <raj266864@gmail.com>
2024-07-18 13:53:47 +0530

    review

--- Reactjs/src/App.jsx
+++ Reactjs/src/App.jsx
@@ -1,35 +1,35 @@
-import React, { useMemo, useState } from "react";
+import React, { lazy, Suspense, useCallback, useMemo, useState } from "react";
 
-function Example() {
+const Component1 = lazy(() => import("./Component1"));
+const Component2 = lazy(() => import("./Component2"));
+
+function App() {
   const [count, setCount] = useState(0);
 
-  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
+  const memoizedValue = useMemo(() => {
+    console.log("Memo value");
+    return count * 2;
+  }, [count]);
 
-  const Increment = () => {
-    console.log("increment");
+  const increment = useCallback(() => {
+    console.log("call back");
     setCount((c) => c + 1);
-  };
-
-  const Maximum = () => {
-    console.log("Maximum");
-    return Math.max(...numbers);
-  };
-
-  const memo = useMemo(() => {
-    return Maximum();
-  }, [numbers]);
+  }, []);
 
   return (
     <div>
       <p>{count}</p>
-      <button onClick={Increment}>Increment</button>
-      {/* <p>{Maximum()}</p>
-       */}
-      <p>Maximum value:{memo}</p>
-      <button onClick={()=>setNumbers([...numbers,count*2])}>Add Numbers</button>
-      <p>{JSON.stringify(numbers)}</p>
+      <button onClick={increment}>Increment</button>
+      <p>memovalue:{memoizedValue}</p>
+
+      <Suspense fallback={<div> Component 1 loading</div>}>
+        <Component1 />
+      </Suspense>
+      <Suspense fallback={<div> Component 2 loading</div>}>
+        <Component2 />
+      </Suspense>
     </div>
   );
 }
 
-export default Example;
+export default App;
